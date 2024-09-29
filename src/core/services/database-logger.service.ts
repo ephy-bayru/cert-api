d5a13c9b0402c4aedb339d73e37923a5bba87f01
ephy-bayru <ephybayru@gmail.com>
2024-09-30 00:26:17 +0300

    refactor: enhance database logging options with validation and warnings using LoggerService

--- src/core/services/database-logger.service.ts
+++ src/core/services/database-logger.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { LoggerService } from 'src/common/services/logger.service';
 import { LoggerOptions, LogLevel } from 'typeorm';
 
 @Injectable()
@@ -14,22 +15,43 @@ export class DatabaseLoggerService {
     'migration',
   ];
 
-  constructor(private configService: ConfigService) {}
+  constructor(
+    private configService: ConfigService,
+    private logger: LoggerService,
+  ) {}
 
   public determineDatabaseLoggingOptions(): LoggerOptions {
-    const loggingConfig = this.configService.get<string>('DB_LOGGING');
+    let loggingConfig =
+      this.configService.get<string>('DB_LOGGING', 'false') ?? 'false';
 
-    if (!loggingConfig || loggingConfig === 'false') {
+    if (typeof loggingConfig !== 'string') {
+      this.logger.logWarn('DB_LOGGING must be a string. Logging is disabled.');
       return false;
-    } else if (loggingConfig === 'true' || loggingConfig === 'all') {
-      return 'all';
-    } else {
-      const options = loggingConfig.split(',');
-      const filteredOptions = options.filter((opt): opt is LogLevel =>
-        this.validOptions.includes(opt as LogLevel),
-      );
-
-      return filteredOptions.length > 0 ? filteredOptions : false;
     }
+
+    loggingConfig = loggingConfig.toLowerCase();
+
+    if (loggingConfig === 'false') {
+      return false;
+    }
+
+    if (['true', 'all'].includes(loggingConfig)) {
+      return 'all';
+    }
+
+    const options = loggingConfig.split(',').map((opt) => opt.trim());
+
+    const filteredOptions = options.filter((opt): opt is LogLevel =>
+      this.validOptions.includes(opt as LogLevel),
+    );
+
+    if (filteredOptions.length === 0) {
+      this.logger.logWarn(
+        `Invalid DB_LOGGING options provided: ${loggingConfig}. Logging is disabled.`,
+      );
+      return false;
+    }
+
+    return filteredOptions;
   }
 }
