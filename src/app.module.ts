5fe5eca1be823e0b71f69ed7ab69c02f4d0eaf51
ephy-bayru <ephybayru@gmail.com>
2024-09-30 00:29:22 +0300

    refactor: integrate global configuration, database, throttling, and logging interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { LoggingInterceptor } from './common/utils/logging.interceptor';
+import { DatabaseModule } from './core/database/database.module';
+import { CommonModule } from './common/common.module';
+import { HealthModule } from './modules/health/health.module';
+import { UsersModule } from './modules/users/users.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+    HealthModule,
+    UsersModule,
+    CommonModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
+  exports: [],
 })
 export class AppModule {}
